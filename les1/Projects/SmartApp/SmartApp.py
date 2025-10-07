c1647e7d40af5e37238e2f80b8dccce35603fcb3
mkn <helloyes.itsme18@gmail.com>
2025-10-07 21:11:43 +0200

    Sprint 1 afgerond, begin sprint 2

--- les1/Projects/SmartApp/SmartApp.py
+++ les1/Projects/SmartApp/SmartApp.py
@@ -2,58 +2,85 @@ def Fahrenheit(temp_celsius):
     return temp_celsius * 1.8 + 32
 
 def gevoelstemperatuur(temp_celsius, windsnelheid, luchtvochtigheid):
-    return (temp_celsius - luchtvochtigheid) / 100 * windsnelheid
-
+    return temp_celsius - (luchtvochtigheid / 100) * windsnelheid
 
 def weerrapport(temp_celcius, windsnelheid, luchtvochtigheid):
     temp_gevoel = gevoelstemperatuur(temp_celcius, windsnelheid, luchtvochtigheid)
 
-    if temp_gevoel > 0 and windsnelheid > 10:
+    if temp_gevoel < 0 and windsnelheid > 10:
         return (f'Het is heel koud en het stormt! Verwarming helemaal aan!')
-    elif temp_gevoel > 0 and windsnelheid < 10:
+    elif temp_gevoel < 0 and windsnelheid <= 10:
         return (f'Het is behoorlijk koud! Verwarming aan op de benedenverdieping!')
     elif 0 <= temp_gevoel < 10 and windsnelheid > 12:
+        return (f'Het is best koud en het waait; verwarming aan en roosters dicht!')
+    elif 0 <= temp_gevoel < 10 and windsnelheid < 12:
         return (f'Het is een beetje koud, elektrische kachel op de benedenverdieping aan!')
     elif 10 <= temp_gevoel < 22:
         return (f'Heerlijk weer, niet te koud of te warm.')
     else:
         return(f'Warm! Airco aan!')
 
-temp_celsius_hist = []  #lijst om gem.temp te pakken
-#temp_celsius_hist.append()
-
 def weerstation():
-    temp_celsius_hist = []  # lijst om gem.temp te pakken
+    temp_celsius_hist = []  # lijst om gem.temp te op te slaan
 
 
     for dag in range(1,8):
 
+#Temperatuur
         while True:
-            temp_celsius = input(f'Hoeveel graden is het op dag {dag}? (°C): ')
-            if temp_celsius == '':
-                print("Leeg vak, probeer opnieuw")
+            temp_1 = input(f'Hoeveel graden is het op dag {dag}? (°C): ')
+            if temp_1 == '':
+                print("Leeg vak, het programma stopt nu.")
+                return
             try:
-                temp_celsius = int()
+                temp_celsius = float(temp_1)
+                break
+
             except ValueError:
-                print('')
+                print('Probeer opnieuw, onjuist type invoer')
 
-
+#Windsnelheid
         while True:
-            windsnelheid = input (f'Wat is de windsnelheid op dag {dag}? (m/s): ')
-            if temp_celsius == '':
-                print("Leeg vak, probeer opnieuw")
+            wind = input (f'Wat is de windsnelheid op dag {dag}? (m/s): ')
+            if wind == '':
+                print("Leeg vak, het programma stopt nu.")
+                return
+            try:
+                windsnelheid = float(wind)
+                break
+            except ValueError:
+                print('Probeer opnieuw, onjuist type invoer')
 
-            luchtvochtigheid = input (f'Wat is de luchtvochtigheid op dag {dag}? (%): ')
-            if temp_celsius == '':
-                print("Leeg vak, probeer opnieuw")
+#Luchtvochtigheid
+        while True:
+            vochtigheid = input (f'Wat is de luchtvochtigheid op dag {dag}? (%): ')
+            if vochtigheid == '':
+                print("Leeg vak, het programma stopt nu.")
+                return
+            try:
+                luchtvochtigheid = float(vochtigheid)
+                if luchtvochtigheid < 0 or luchtvochtigheid > 100:
+                    print('Invoer moet tussen 0-100 liggen, probeer nogmaals.')
+                    continue
+                break
+            except ValueError:
+                print('Probeer opnieuw, onjuist type invoer')
 
-            dag += 1
-            print("=======================================================================")
-            temp_celsius_hist.append(temp_celsius)
 
-   # if temp_celsius:
+        temp_celsius_hist.append(temp_celsius)
+        gem_temp = sum(temp_celsius_hist) / len(temp_celsius_hist)
+        print(f'Het is {temp_celsius:.1f}C en {Fahrenheit(temp_celsius):.1f}F')
+        print(weerrapport(temp_celsius, windsnelheid, luchtvochtigheid))
+        print(f'De gemiddelde temperatuur is vandaag {gem_temp:.1f}C')
+        print("=======================================================================")
+
+def aantal_dagen(inputFile):
+    with open(inputFile) as bestand:
+        regels = bestand.readlines()
+        dagen = len(regels) - 1
+        return dagen
+
+def auto_bereken(inputFile, outputFile):
 
 
-
-# IF temp_celsius or windsnelheid or luchtvochtigheid == ' ' BREEEAAAAKKK
-
+def overwrite_settings(outputFile):
